5e4c8be503c13f830d53f7292afcd505bb7ad52b
Julio Campagnolo <juliocampagnolo@gmail.com>
2019-03-30 11:51:37 -0300

    fighting to build

--- astrometry.net/build.sh
+++ astrometry.net/build.sh
@@ -10,18 +10,13 @@ export CFITS_LIB="-L$CONDA_PREFIX/lib -lcfitsio -lm"
 export NETPBM_INC="-I$CONDA_PREFIX/include"
 export NETPBM_LIB="-L$CONDA_PREFIX/lib -lnetpbm"
 export CAIRO_INC="-I$CONDA_PREFIX/include -I$CONDA_PREFIX/include/cairo"
-export CAIRO_LIB="-L$CONDA_PREFIX/lib -lcairo -lXrender"
+export CAIRO_LIB="-L$CONDA_PREFIX/lib -lcairo"
 export PNG_INC="-I$CONDA_PREFIX/include"
 export PNG_LIB="-L$CONDA_PREFIX/lib -lpng16"
 export JPEG_INC="-I$CONDA_PREFIX/include"
 export JPEG_LIB="-L$CONDA_PREFIX/lib -ljpeg"
 export ZLIB_INC="-I$CONDA_PREFIX/include"
 export ZLIB_LIB="-L$CONDA_PREFIX/lib -lz"
-#export WCSLIB_INC="-I$CONDA_PREFIX/include/wcslib -I$CONDA_PREFIX/include"
-#export WCSLIB_LIB="-L$CONDA_PREFIX/lib -lwcs"
-#export SYSTEM_GSL=yes
-#export GSL_INC="-I$CONDA_PREFIX/include -I$CONDA_PREFIX/include/gsl"
-#export GSL_LIB="-L$CONDA_PREFIX/lib -lgsl -lblas"
 
 make
 make extra
